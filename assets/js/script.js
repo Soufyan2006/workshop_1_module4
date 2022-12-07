1f5bf8471eaa5d367e820d2ef52f3dbdf0a138d8
Soufyan2006 <113595059+Soufyan2006@users.noreply.github.com>
2022-12-07 13:05:34 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var eersteVariabele = "test";
+console.log(eersteVariabele)
+
+var voornaam = "Soufyan"; // wijzig dit in jouw eigen voornaam
+var achternaam = "Zeamari"; // wijzig dit in jouw eigen achternaam
+var volledigeNaam = voornaam + achternaam; // plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var geboortedag = "26/"; // je geboortedag
+var geboortemaand = "08/"; // je geboortemaand
+var geboortejaar = "2006/"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
+var leeftijd = 16; // bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var dag_van_het_jaar = 6; // bereken hier hoeveelste dag van dit kalenderjaar het is
+var straat = "walvraan van hallaan"; // je straat;
+var huisnummer = "4"; // je huisnummer;
+var woonplaats = "Gouda"; // je woonplaats;
+var woonachtig_in_utrecht = ""; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+var aantal_studenten_in_deze_klas = 20;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 35; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+alert(voornaam+ achternaam + geboortedag+ geboortejaar+ geboortemaand + huidig_jaartal + leeftijd + dag_van_het_jaar + straat + huisnummer + woonplaats + woonachtig_in_utrecht + aantal_studenten_in_deze_klas + kosten_van_frikandelbroodje + totale_lunchkosten)
